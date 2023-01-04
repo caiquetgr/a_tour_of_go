8ae511de73ad577e2e900135be1d9cae9b3c6f5a
Caique Borges <caiquetgr@gmail.com>
2023-01-04 18:50:43 -0300

    a tour of go: generics

---
+++ a_tour_of_go_list/generics/generic_types.go
@@ -0,0 +1,6 @@
+package main
+
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
---
+++ a_tour_of_go_list/generics/type_parameters.go
@@ -0,0 +1,22 @@
+
+package main
+
+import "fmt"
+
+func Index[T comparable](s []T, x T) int {
+	for i, v := range s {
+		// v and x are type T, which has the comparable constraint, so we can use == here.
+		if v == x {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	si := []int{10, 20, 15, -10}
+	fmt.Println(Index(si, 15))
+
+	ss := []string{"foo", "bar", "baz"}
+	fmt.Println(Index(ss, "hello"))
+}
